3ab776f7123fb3ac7b8f385e679b55f2257a107c
Thomas Orlans <totom.orlans@gmail.com>
2022-04-25 19:44:46 +0200

    register validation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
 import Home from './components/frontend/Home';
 import Login from './components/frontend/auth/Login';
 import Register from './components/frontend/auth/Register';
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+axios.defaults.withCredentials = true;
+
 
 function App() {
   
--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,7 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import Navbar from "../../../layouts/frontend/Navbar";
+import swal from "sweetalert";
+import {useNavigate} from "react-router-dom";
 
 function Register(){
+
+    const Navigate = useNavigate();
+
+    const [registerInput, setRegister] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error_list: [],
+    });
+    
+    const handleInput = (e) =>{
+        e.persist();
+        setRegister({...registerInput,[e.target.name]: e.target.value})
+    }
+
+    const registerSubmit = (e) =>{
+        e.preventDefault();
+
+        const data = {
+            name: registerInput.name,
+            email: registerInput.email,
+            password: registerInput.password,
+        }
+
+        axios.get('/sanctum/csrf-cookie').then(response=>{
+        axios.post(`/api/register`, data).then(res =>{
+                if(res.data.status === 200){
+                    localStorage.setItem('auth_token',res.data.token);
+                    localStorage.setItem('auth_name',res.data.username);
+                    swal("Succes", res.data.message, "Succes");
+                    Navigate("/");
+                }else{
+                    setRegister({...registerInput, error_list: res.data.validation_errors})
+                }
+            });
+        });
+    }
+
     return(
         <div>
             <Navbar/>
@@ -13,22 +54,21 @@ function Register(){
                                 <h4>Register</h4>
                             </div>   
                             <div className="card-body">
-                                <form>
+                                <form onSubmit={registerSubmit}>
                                     <div className="form-group mb-3">
                                         <label>Name</label>
-                                        <input type="" name="name" className="form-control" value="" />
+                                        <input type="" name="name" value={registerInput.name} onChange={handleInput}  className="form-control"  />
+                                        <span className="text-danger">{registerInput.error_list.name}</span>
                                     </div>
                                     <div className="form-group mb-3">
                                         <label>Email</label>
-                                        <input type="" name="email" className="form-control" value="" />
+                                        <input type="" name="email" value={registerInput.email} onChange={handleInput}  className="form-control" />
+                                        <span className="text-danger">{registerInput.error_list.email}</span>
                                     </div>
                                     <div className="form-group mb-3">
                                         <label>Password</label>
-                                        <input type="" name="password" className="form-control" value="" />
-                                    </div>
-                                    <div className="form-group mb-3">
-                                        <label>Confirm Password</label>
-                                        <input type="" name="confirm_password" className="form-control" value="" />
+                                        <input type="" name="password" value={registerInput.password} onChange={handleInput}  className="form-control"  />
+                                        <span className="text-danger">{registerInput.error_list.password}</span>
                                     </div>
                                     <div className="form-group mb-3">
                                         <button type="submit" className="btn btn-primary">Register</button>
