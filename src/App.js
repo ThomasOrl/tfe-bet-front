f7b80a73a9d9d8b3dd0cd6bf4b9d9321df500716
Thomas Orlans <totom.orlans@gmail.com>
2022-04-26 20:44:52 +0200

    logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route ,Routes, Navigate, Redirect } from 'react-router-dom';
 
 import Home from './components/frontend/Home';
 import Login from './components/frontend/auth/Login';
@@ -11,18 +11,30 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
 
 axios.defaults.withCredentials = true;
-
+axios.interceptors.request.use(function(config){
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
 
 function App() {
-  
   return (
     <div className="App">
       <Router>
         <Routes>
 
           <Route exact path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
+          {/* <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>  */}
+
+          <Route path="/login"
+            element={localStorage.getItem('auth_token') ? <Navigate to='/'/> : <Login/>}
+          />
+          <Route path="/register"
+            element={localStorage.getItem('auth_token') ? <Navigate to='/'/> : <Register/>}
+          />
+
+
 
         </Routes>
       </Router>
--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -1,7 +1,47 @@
+import axios from "axios";
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import swal from "sweetalert";
 
 function Navbar(){
+
+    const Navigate = useNavigate();
+
+    const logoutSubmit = (e) =>{
+        e.preventDefault();
+
+        axios.post(`/api/logout`).then(res =>{
+            if(res.data.status === 200){
+                localStorage.removeItem('auth_token');
+                localStorage.removeItem('auth_name');
+                swal("Succes", res.data.message, "Succes");
+                Navigate("/");
+            }
+
+        });
+    }
+
+    var AuthButtons = '';
+    if(!localStorage.getItem('auth_token')){
+        AuthButtons = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/register">Register</Link>
+                </li>
+            </ul>
+        );
+    }else{
+        AuthButtons = (
+            <li className="nav-item">
+                <button type="button" onClick={logoutSubmit} className="nav-link btn btn-danger btn-sm text-white" >Logout</button>
+            </li>
+        );
+
+    }
+
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow sticky-top">
             <div className="container">
@@ -17,13 +57,9 @@ function Navbar(){
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
+                        {AuthButtons}
                         
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
+                        
                     </ul>
                 </div>
             </div>
